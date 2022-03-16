2ddae9b12190fc8465d7adfbb8cbfdb5bff0b4ab
Pettya47 <petq_1002@abv.bg>
2022-03-16 15:10:34 +0200

    Max

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -9,5 +9,6 @@ int main()
 {
     int a[] = { 1,3,2,5,4,7,6,3 };
     cout << "Suma=" << suma(a, 8);
+    cout << "Max=" << max(a[4], a[7]);
 }
 
--- ConsoleApplication1/ConsoleApplication1/FuncDef.h
+++ ConsoleApplication1/ConsoleApplication1/FuncDef.h
@@ -1,3 +1,6 @@
 #pragma once
 
 int suma(int a[], int br);
+int suma(int a[], int br);
+int multiply(int a, int b);
+int max(int a, int b);
